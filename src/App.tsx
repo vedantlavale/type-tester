7912dadf797d28e45e6a565ee1fc58d91c399ac7
vedant <vedantlavale@gmail.com>
2025-04-02 09:59:38 +0530

    Add faker for generating random words and update styles for a centered layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {faker} from '@faker-js/faker'
+
 function App() {
+
+const words = faker.lorem.words(30)
 
   return (
     <>
-    
+     <h1 className="text-primary-500">{words}</h1>
     </>
   )
 }
